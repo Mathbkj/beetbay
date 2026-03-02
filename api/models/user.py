e2351afc1147e6e287fa9d0bc411211fa8327ad6
Mathbkj <matheusgblasel@hotmail.com>
2026-03-02 20:10:29 -0300

    patch:1.0.1

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,8 @@
 
-from db import db
+from models.db import database
 from sqlalchemy.orm import Mapped, mapped_column
-class User(db.Model):
+
+class User(database.Model):
     id:Mapped[int] = mapped_column(primary_key=True)
     username:Mapped[str] = mapped_column(unique=True)
     password:Mapped[str]
